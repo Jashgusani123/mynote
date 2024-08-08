f51bfe66b09f2def21aa271bd35b052f3ab1ba64
JASH GUSANI~ <jashgusani20@gmail.com>
2024-08-08 22:40:06 +0530

    changes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const connectToMongo = require('./DataBaseConect'); 
 var cors = require('cors')
-const path = require('path');
+// const path = require('path');
 
 connectToMongo();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'mynote/build')));
+// app.use(express.static(path.join(__dirname, 'mynote/build')));
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'mynote', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'mynote', 'build', 'index.html'));
+// });
 
 
 
