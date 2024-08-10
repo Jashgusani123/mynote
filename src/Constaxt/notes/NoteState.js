e59e8ebbb9782c17266812d838ec3fd5e0c107f7
JASH GUSANI~ <jashgusani20@gmail.com>
2024-08-10 19:46:06 +0530

    a1

--- src/Constaxt/notes/NoteState.js
+++ src/Constaxt/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import noteContext from './noteContext'
 
 const NoteState = (props) => {
-  const host = process.env.REACT_APP_API_URL
+  const host = "http://localhost:5000"
    const noteInitsialized = []
    const [notes, setnotes] = useState(noteInitsialized)
 
